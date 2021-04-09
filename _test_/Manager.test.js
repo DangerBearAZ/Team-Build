c3e949d1672d4a77d65d8b95f7fe9809502a050e
claire ogesen <claire.ogesen.co@gmail.com>
2021-04-09 09:32:57 -0700

    manager test work not sure why it works but that is OK

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -2,6 +2,7 @@ const { expect, test } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
 test("the getRole() function should overide and return manager", () => {
-    const employee = new Manager('Claire', '12A', 'email', 'office'); 
+    const employee =  new Manager('Claire', '12A', 'email', 'office'); 
     expect(employee.getRole()).toBe("Manager")
 });
+
