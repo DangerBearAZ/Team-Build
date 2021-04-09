b1ca25ca2fbc2d36b3453e44e44077cae367a652
claire ogesen <claire.ogesen.co@gmail.com>
2021-04-08 19:41:36 -0700

    employee test passing

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -2,11 +2,11 @@ const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('creates a new employee object', () => {
-    const employee = new Employee('');
+    const employee = new Employee('Claire', '12A', 'email', 'office');
 
-    expect(employee.name).toBe('');
-    expect(employee.id).toBe('');
-    expect(eployee.email).toBe('');
-    expect(employee.office).toBe('');
+    expect(employee.name).toBe('Claire');
+    expect(employee.id).toBe('12A');
+    expect(employee.email).toBe('email');
+    expect(employee.office).toBe('office');
 });
 
