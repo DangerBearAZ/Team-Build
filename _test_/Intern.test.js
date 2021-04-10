946c484ae25492f08184b49e6cc71f3be13892c2
claire ogesen <claire.ogesen.co@gmail.com>
2021-04-10 09:13:25 -0700

    intern test not working ReferenceError: school is not defined

--- _test_/Intern.test.js
+++ _test_/Intern.test.js
@@ -2,7 +2,7 @@ const Intern = require('../lib/Intern');
 
 test("add school to employee", () =>  {
 const employee = new Intern('Claire', '12A', 'email', 'school')
-expect (employee.school).tobe(school);
+expect (employee.school).toBe(school);
 });
 
 // test("the getRole() function should overide and return intern", () => {
